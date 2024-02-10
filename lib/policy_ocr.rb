d7f9b3e411635794201c0be59b7b1087b426b750
Igor Taskovski <igor.taskovski@gmail.com>
2024-02-10 16:39:33 -0500

    Implements User Story 1 - Logic for parsing policy numbers

--- lib/policy_ocr.rb
+++ lib/policy_ocr.rb
@@ -1,2 +1,59 @@
 module PolicyOcr
+  class PolicyNumberParser
+    require 'pry'
+
+    DIGIT_MAP = {
+      " _ | ||_|" => 0,
+      "     |  |" => 1,
+      " _  _||_ " => 2,
+      " _  _| _|" => 3,
+      "   |_|  |" => 4,
+      " _ |_  _|" => 5,
+      " _ |_ |_|" => 6,
+      " _   |  |" => 7,
+      " _ |_||_|" => 8,
+      " _ |_| _|" => 9
+    }
+
+    def initialize(file_path)
+      @file_path = file_path
+    end
+
+    # Parse the policy numbers from the file
+    def parse_policy_numbers
+      policy_numbers = []
+
+      # Read the file line by line and parse each policy number
+      File.foreach(@file_path).each_slice(4) do |policy_number|
+        policy_numbers << parse_single_number(policy_number)
+      end
+      policy_numbers
+    end
+
+    private
+
+    # Parse a single policy number
+    def parse_single_number(policy_number)
+      policy_number.pop
+      placeholder = Array.new(9, '')
+
+      policy_number.each do |line|
+        line.scan(/.{3}/).each_with_index do |digit, i|
+          placeholder[i] += digit
+        end
+      end
+
+      digits = ""
+
+      placeholder.each do |p|
+        if DIGIT_MAP.key?(p)
+          digits += DIGIT_MAP[p].to_s
+        else
+          digits += "?"
+        end
+      end
+
+      digits
+    end
+  end
 end
--- spec/policy_ocr_spec.rb
+++ spec/policy_ocr_spec.rb
@@ -1,11 +1,27 @@
 require_relative '../lib/policy_ocr'
 
-describe PolicyOcr do
-  it "loads" do
-    expect(PolicyOcr).to be_a Module
+describe PolicyOcr::PolicyNumberParser do
+  let(:file_path) { File.expand_path("../fixtures/sample.txt", __FILE__) }
+  let(:parser) { described_class.new(file_path) }
+
+  context "file loading" do
+    it "loads" do
+      expect(PolicyOcr).to be_a Module
+    end
+
+    it 'loads the sample.txt' do
+      expect(fixture('sample').lines.count).to eq(44)
+    end
   end
 
-  it 'loads the sample.txt' do
-    expect(fixture('sample').lines.count).to eq(44)
+  context "parsing the policy numbers" do
+    it "parses the numbers correctly" do
+      expect(parser.parse_policy_numbers).to eq(["000000000", "111111111", "222222222", "333333333", "444444444", "555555555", "666666666", "777777777", "888888888", "999999999", "123456789"])
+    end
+
+    it "parses each number individually" do
+      parsed_numbers = parser.parse_policy_numbers
+      expect(parsed_numbers).to include("123456789")
+    end
   end
 end
